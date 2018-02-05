e4c17b5a0d38ce97c218cb3635c5e0474363e092
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-02-05 21:30:27 +0000

    Init repo

---
+++ main.lua
@@ -0,0 +1,27 @@
+--[[
+    =======================
+    rm -rf .git
+    =======================
+]]--
+
+local Grid = require("src.grid")
+
+local grid
+
+function love.load()
+    grid = Grid.create()
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    grid:draw()
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
